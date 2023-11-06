85e72efaf59dfd0ff5e8fda9b6693471bb6dcbde
vincent van de merwe <335479@student.mboutrecht.nl>
2023-11-06 14:05:06 +0100

    added a view for the index

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    //* public function index(): Response
+    //* {
+    //*     return response('Hello, world');
+    //* }
+    public function index(): View
     {
-        //
+        return view('chirps.index');
     }
 
     /**
